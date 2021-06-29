d9b1f1fd32b3e642f5ac8a101bfd09e58a861f65
minhquoce <minhquoc1312200@gmail.com>
2021-06-29 20:11:00 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  
+    React.createElement('h1',null,"hello word"),
+  document.getElementById('root')
+);
+const element = <h1>Hello, world!</h1>;
+
+
